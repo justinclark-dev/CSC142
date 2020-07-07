7e7ed95f26ce48a7ae069d4e41b0281e670fa1f4
justinclark-dev <justinclark.dev@gmail.com>
2020-07-07 09:50:46 -0700

    Class example.

--- week_two/ClassStructure/Counter.java
+++ week_two/ClassStructure/Counter.java
@@ -1,33 +1,48 @@
-/** 
- * A simple integer counter
+/**
+ * A simple integer counter.
  */
 public class Counter {
-
+    
+    // This stores the current value for the counter.  
     private int count;
-
+    
     /**
      * Create a new Counter, with count initialized to 0.
      */
     public Counter() {
         count = 0;
     }
-
-    /** 
+    
+    /**
      * The number of items counted.
+     *
+     * @return The current count.
      */
-    public int currentCount () {
+    public int getCount() {
         return count;
     }
-
+    
     /**
      * Increment the count by 1.
      */
-    public void incrementCount() {
-        count++;
+    public void increment() {
+        count = count + 1;
     }
-
-    /** Reset the count to 0. */
+    
+    /**
+     * Increment the count by the given amount.
+     *
+     * @param val The amount for the increment
+     */
+    public void increment(int val) {
+        count = count + val;
+    }
+    
+    /**
+     * Reset the count to zero.
+     */
     public void reset() {
         count = 0;
     }
+    
 }
