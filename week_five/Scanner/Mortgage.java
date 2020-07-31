1889bf541ed2b0b7c946a8449281a5b3655cdc50
justinclark-dev <justinclark.dev@gmail.com>
2020-07-31 00:00:44 -0700

    console app for calculating mortage

---
+++ week_five/Scanner/Mortgage.java
@@ -0,0 +1,27 @@
+
+// This program prompts for information about a loan and
+// computes the monthly mortgage payment.
+import java.util.*; // for Scanner
+
+public class Mortgage {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        // obtain values
+        System.out.println("This program computes monthly " +
+        "mortgage payments.");
+        System.out.print("loan amount : ");
+        double loan = console.nextDouble();
+        System.out.print("number of years : ");
+        int years = console.nextInt();
+        System.out.print("interest rate : ");
+        double rate = console.nextDouble();
+        System.out.println();
+
+        // compute result and report
+        int n = 12 * years;
+        double c = rate / 12.0 / 100.0;
+        double payment = loan * c * Math.pow(1 + c, n) / (Math.pow(1 + c, n) - 1);
+        System.out.println("payment = $" + Math.round(payment));
+    }
+}
